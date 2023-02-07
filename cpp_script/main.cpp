b82e40028e25632cf6ae9e6e093148b7d8b28dfa
nail <khalifa.nail@gmail.com>
2023-02-07 16:10:33 +0100

    actualisation de certain fichier

--- cpp_script/main.cpp
+++ cpp_script/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     matrice M = matrice(10,10);
     cout << M;
-    save_matr("mat.txt",M);
+    //save_matr("../../py_script/mat.txt",M);
+    //test
     return 0;
 }
--- py_script/README.md
+++ py_script/README.md
@@ -1,1 +1,3 @@
-# C++ script
+# Python script
+
+Fonction read qui permet de lire une matrice dans un fichier texte
