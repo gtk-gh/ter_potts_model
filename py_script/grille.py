b5bb2b85f9bdba0594bd9f2f4fdbd808473f8042
nail <khalifa.nail@gmail.com>
2023-02-07 16:22:41 +0100

    création de grille.py

---
+++ py_script/grille.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from read_matrice import read
+
+array = read("mat.txt")
+x = np.arange(0, len(array))  # vecteur x
+y = np.arange(0, len(array[0]))  # vecteur y
+cmap = plt.get_cmap('YlOrRd') # choix du set de couleur
+
+
+fig, ax = plt.subplots(nrows=1, ncols=1)
+# Affectation des états
+
+# On plot la matrice avec un état = une couleur sur la grille
+colormesh = ax.pcolor(x, y, array, vmin=0, vmax=2, cmap=cmap,linewidths=1)
+
+
+cbar = fig.colorbar(colormesh,aspect = 1) # configuration de la colorbar pour afficher les états
+
+plt.show()
