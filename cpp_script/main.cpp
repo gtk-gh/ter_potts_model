7cc978395c2c48b24f0dea4dd07ddd3930e9af6a
nail <khalifa.nail@gmail.com>
2023-05-19 11:49:27 +0200

    Metropolis hasting

--- cpp_script/main.cpp
+++ cpp_script/main.cpp
@@ -7,26 +7,29 @@ int main() {
     srand (time(NULL));
 
     vector<double> etat({0,1});
-    int size = 7;
+    int size = 10;
 
     vector<int> simil;
     vector<double> som;
 
-    for (int i = 0; i < size*size; i++){
-        int tempsim = 0;
-        double tempsom = 0;
-        if (rand()%2 >= 1){
-            tempsim =1;
-            tempsom =1;
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j< size ; j++){
+            int tempsim = 1;
+            double tempsom = 0;
+            if ((i <= size/2) && (j <= size/2)){
+                tempsim =2;
+                tempsom =1;
+            }
+            simil.push_back(tempsim);
+            som.push_back(tempsom);
         }
-        simil.push_back(tempsim);
-        som.push_back(tempsom);
+
     }
     //cout << som[30];
 
     matrice M = matrice(size,size,som,etat);
     //cout << M;
-    int n = 100;
+    int n = 1000;
     vector<matrice> MH1;
     MH1 = mh1(n,M,290,simil);
 
